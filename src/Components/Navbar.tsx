9a3d4be006981bbd92e571dce261f3f9f8177c2c
Betty <bhetea001@gmail.com>
2026-06-19 22:29:37 +0100

    Create Scoreboard page

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -7,6 +7,7 @@ export default function Navbar() {
                 <Link href="/" className="hover:text-white">Home</Link>
                 <Link href="/gallery" className="hover:text-white">Gallery</Link>
                 <Link href="/quiz" className="hover:text-white">Quiz</Link>
+                <Link href="/scoreboard" className="hover:text-white">Scoreboard</Link>
             </div>
         </nav>
     )
